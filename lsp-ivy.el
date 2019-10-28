78c1429c62c19006058b89d462657e1448d1e595
Sebastian Sturm <s.sturm@arkona-technologies.de>
2019-10-28 10:02:45 +0100

    Add Commentary section

--- lsp-ivy.el
+++ lsp-ivy.el
@@ -24,7 +24,9 @@
 
 ;;; Commentary:
 
-;;
+;; This package provides an interactive ivy interface to the workspace symbol
+;; functionality offered by lsp-mode. For an alternative implementation based on
+;; helm, see https://github.com/emacs-lsp/helm-lsp
 
 ;;; Code:
 
